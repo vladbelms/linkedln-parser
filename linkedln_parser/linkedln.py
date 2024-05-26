debbad7a8f3304d8623a0907f784a28a5a749f33
Vlad <vladbelms@gmail.com>
2024-05-26 15:35:04 +0300

    version 1.0

--- linkedln_parser/linkedln.py
+++ linkedln_parser/linkedln.py
@@ -1,2 +1,14 @@
+import httpx
+from bs4 import BeautifulSoup
+
+
 class Linkedln:
-    pass
+    def __init__(self,URL):
+        self.URL = URL
+    def get_name(self):
+        page = httpx.get(self.URL)
+        results = BeautifulSoup(page.content, "html.parser")
+        job_elements = results.find_all("h3", class_="base-search-card__title")
+        for job_element in job_elements:
+            print(job_element.text.strip())
+
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from linkedln_parser import Linkedln
 
 
 def main():
-    pass
+    url = Linkedln("https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0")
+    url.get_name()
 
 
 if __name__ == '__main__':
