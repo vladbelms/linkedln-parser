66a95207d224285327eb73886fc31c88bf1820d2
Vlad <vladbelms@gmail.com>
2024-06-01 20:14:57 +0300

    version 1.1

--- linkedln_parser/linkedln.py
+++ linkedln_parser/linkedln.py
@@ -3,12 +3,17 @@ from bs4 import BeautifulSoup
 
 
 class Linkedln:
-    def __init__(self,URL):
-        self.URL = URL
-    def get_name(self):
-        page = httpx.get(self.URL)
-        results = BeautifulSoup(page.content, "html.parser")
-        job_elements = results.find_all("h3", class_="base-search-card__title")
-        for job_element in job_elements:
-            print(job_element.text.strip())
+    def __init__(self, keywords: str, location: str):
+        self.keywords = keywords.replace(" ", "%20")
+        self.location = location.replace(" ", "%20")
 
+    def get_name(self) -> None:
+        link = f"https://www.linkedin.com/jobs/search?keywords={self.keywords}&location={self.location}trk=guest_homepage-basic_guest_nav_menu_jobs"
+        page = httpx.get(link)
+        results = BeautifulSoup(page.content, "html.parser")
+        name_companies = results.find_all("h3", class_="base-search-card__title")
+        country_companies = results.find_all("h4", class_="base-search-card__subtitle")
+        for name_company in name_companies:
+            for country_company in country_companies:
+                job_stats = [name_company.text.strip(), country_company.text.strip()]
+                print(job_stats)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from linkedln_parser import Linkedln
 
 
 def main():
-    url = Linkedln("https://www.linkedin.com/jobs/search?trk=guest_homepage-basic_guest_nav_menu_jobs&position=1&pageNum=0")
+    url = Linkedln("", "")
     url.get_name()
 
 
