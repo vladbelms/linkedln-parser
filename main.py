6eeedacd882f481b4a716354eb6edf0f0b689ac4
vlad_belms <vladbelms@gmail.com>
2024-07-10 20:17:35 +0300

    full update menu.py and change logic of main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from src import Menu, Linkedln
 
 def main():
     menu = Menu()
-    menu.show_base_info("Linkedln Parser")
-    number = menu.get_menu_data("Print a positive number")
-    url = Linkedln([""], ["USA"])
-    list_ = url.get_linkedln_data(number)
-    menu.show_list(list_)
-
+    region = menu.region
+    vacancies = menu.vacancies
+    url = Linkedln([""], [region])
+    list_ = url.get_linkedln_data(vacancies)
+    menu.all_data(list_)
 
 if __name__ == '__main__':
     main()
--- src/menu_app/menu.py
+++ src/menu_app/menu.py
@@ -1,43 +1,114 @@
+import sys
 from blessed import Terminal
 
 
 class Menu:
-    def show_base_info(self, project_name: str) -> None:
-        term = Terminal()
-        with term.fullscreen(), term.cbreak():
-            print(term.home + term.clear)
-            print(f"{term.bold}it's a {project_name}{term.normal}")
-            print("\n\n" + term.italic + "Press any key to exit..." + term.no_italic)
-            term.inkey()
 
-    def get_menu_data(self, promt: str) -> int:
-        term = Terminal()
-        print(term.clear)
-        with term.cbreak():
-            promt = str(promt)
-            print(term.move_y(term.height // 2) + term.center(promt), end='', flush=True)
+    def __init__(self):
+        self.__region = 'USA'
+        self.__vacancies = 5
+        self.__term = Terminal()
 
-            number = ''
-            while True:
-                key = term.inkey()
-                if key.isdigit():
-                    number += key
-                    print(key, end='', flush=True)
-                elif key.code == term.KEY_ENTER:
-                    break
-        return int(number)
+    @property
+    def region(self):
+        return self.__region
+
+    @property
+    def vacancies(self):
+        return self.__vacancies
+
+    @property
+    def term(self):
+        return self.__term
 
     def show_list(self, list_: list) -> None:
+        start_index = 0
+        end_index = min(len(list_), self.__term.height - 4)
 
-        term = Terminal()
-        with term.fullscreen(), term.cbreak():
-            print(term.clear)
-            print(term.move_y(term.height // 2 - len(list_) // 2))
+        with self.__term.cbreak(), self.__term.hidden_cursor():
+            while True:
+                print(self.__term.clear())
+                print(self.__term.move_y(2))
+                print(self.__term.move_x(2) + 'Vacancies List')
 
-            for dictionary in list_:
-                for key, value in dictionary.items():
-                    print(f"{key}: {value}")
-                print(term.horizontal_line(term.width))
+                for i in range(start_index, end_index):
+                    dictionary = list_[i]
+                    for key, value in dictionary.items():
+                        print(f"{key}: {value}")
+                    print(self.__term.horizontal_line(self.__term.width))
 
-            print(term.move(term.height - 1, 0) + "Press any key to exit...")
-            term.inkey()
+                print(self.__term.move_y(self.__term.height - 2))
+                print(self.__term.center('Press UP/DOWN to scroll, ENTER to return'))
+
+                val = self.__term.inkey()
+
+                if val.name == 'KEY_UP' and start_index > 0:
+                    start_index -= 1
+                    end_index -= 1
+                elif val.name == 'KEY_DOWN' and end_index < len(list_):
+                    start_index += 1
+                    end_index += 1
+                elif val.name == 'KEY_ENTER':
+                    break
+
+    def settings(self):
+        regions = ['UK', 'USA', 'EU']
+        region_index = regions.index(self.__region)
+
+        while True:
+            print(self.__term.clear())
+            print(self.__term.move_y(self.__term.height // 2 - 2))
+            print(self.__term.center('Settings Menu'))
+            print(self.__term.move_y(self.__term.height // 2 - 1))
+            print(self.__term.center(f'Region: {regions[region_index]}'))
+            print(self.__term.move_y(self.__term.height // 2))
+            print(self.__term.center(f'Vacancies: {self.__vacancies}'))
+            print(self.__term.move_y(self.__term.height // 2 + 2))
+            print(self.__term.center(
+                'Press UP/DOWN to change region, LEFT/RIGHT to change vacancies, ENTER to save and return'))
+
+            val = self.__term.inkey()
+
+            if val.name == 'KEY_UP':
+                region_index = (region_index - 1) % len(regions)
+            elif val.name == 'KEY_DOWN':
+                region_index = (region_index + 1) % len(regions)
+            elif val.name == 'KEY_LEFT':
+                self.__vacancies = max(1, self.__vacancies - 1)
+            elif val.name == 'KEY_RIGHT':
+                self.__vacancies += 1
+            elif val.name == 'KEY_ENTER':
+                self.__region = regions[region_index]
+                break
+
+    def all_data(self,list_: list) -> None:
+        current_option = 0
+        options = ['Parse', 'Settings', 'Exit']
+
+        with self.__term.fullscreen(), self.__term.cbreak(), self.__term.hidden_cursor():
+            while True:
+                print(self.__term.clear())
+                print(self.__term.move_y(2))
+                print(self.__term.move_x(2) + 'Linkedin Parser')
+                for i, option in enumerate(options):
+                    print(self.__term.move_y(4 + i))
+                    if i == current_option:
+                        print(self.__term.move_x(2) + self.__term.on_blue(self.__term.bold(f'> {option} ')))
+                    else:
+                        print(self.__term.move_x(2) + f'  {option} ')
+
+                val = self.__term.inkey()
+
+                if val.name == 'KEY_UP':
+                    current_option = (current_option - 1) % len(options)
+                elif val.name == 'KEY_DOWN':
+                    current_option = (current_option + 1) % len(options)
+                elif val.name == 'KEY_ENTER':
+                    if current_option == 0:
+                        self.show_list(list_)
+                    elif current_option == 1:
+                        self.settings()
+                    elif current_option == 2:
+                        print(self.__term.clear())
+                        return
+
